73b45c3afd5d19dc11e44f8b2e018d5692cb3511
Mayank Verma <67826485+mayank-verma-cs19@users.noreply.github.com>
2020-10-30 20:26:16 +0530

    added cases in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -16,6 +16,23 @@ public class Tortoise extends Thread  {
     @Override
     public void run() {
         for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
 
         }
     }
