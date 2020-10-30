14fec52aad48ce6a3dd034c55928ec570c23935b
Mayank Verma <67826485+mayank-verma-cs19@users.noreply.github.com>
2020-10-30 20:28:15 +0530

    added dialog box in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,5 +1,7 @@
 package thread.tortoise;
 
+import javax.swing.*;
+
 public class Tortoise extends Thread  {
     private static final int MILESTONES = 5;
 
@@ -38,6 +40,13 @@ public class Tortoise extends Thread  {
             } catch (InterruptedException e) {
                 System.err.println("The Tortoise has lost the path!");
             }
+            JOptionPane.showConfirmDialog(
+                    null,
+                    "The Tortoise has completed the race!",
+                    "Tortoise",
+                    JOptionPane.OK_CANCEL_OPTION
+            );
+
 
         }
     }
