b76a090032046c5146832d45e4ae3b1f8f36a4c1
Mayank Verma <67826485+mayank-verma-cs19@users.noreply.github.com>
2020-10-30 20:26:57 +0530

    added try and catch in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -33,6 +33,11 @@ public class Tortoise extends Thread  {
                     System.out.println("The Tortoise has reached the finish line!");
                     break;
             }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has lost the path!");
+            }
 
         }
     }
